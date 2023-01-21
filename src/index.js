5d01d865905aa8415931cdb3254c7036683fe689
HelenaUa <delena.1408@gmail.com>
2023-01-21 18:19:19 +0200

    add no

--- src/index.js
+++ src/index.js
@@ -13,36 +13,83 @@ const btnLoadMore = document.querySelector(".load-more");
 const KEY = "32997902-3b59b8944b64f8408d8a5fafd";
 const BASE_URL = "https://pixabay.com/api/";
 
-form.addEventListener("input", onSubmitForm);
+ const page = 1;
+
+form.addEventListener("submit", onSubmitForm);
 
 function onSubmitForm(event) {
-    event.preventDefault();
-    const nameSearch = event.target.value.trim();
-
+  btnLoadMore.hidden = true;
+  event.preventDefault();
+  
+  const nameSearch = event.target.elements.searchQuery.value.trim();
+  
     divGallery.innerHTML = "";
 
     if (!nameSearch) {
         return;
     };
-
-fetchTerm(nameSearch)
+  
+  fetchTerm(nameSearch, page).then(data => {
+    
+    
+    const arrayPictures = createMarkupImg(data.hits);
+    divGallery.insertAdjacentHTML("beforeend", arrayPictures);
+    btnLoadMore.hidden = false;
+}).catch(error => console.log(error));
 
 };
 
 
-async function fetchTerm(name) {
-    const page = 1;
-try {
+async function fetchTerm(name, page) {
 const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
    console.log(response);
     if (!response.ok) {
-        throw new Error(response.statusText)
-  };
-    return await response.json();
-    page += 1;
-    
-  } catch (error) {
-    console.log(error);
-  };
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    };
+
+    //  const data = await response.json();
+  page += 1;
+  return;
 };
+
+
+
+function createMarkupImg(array) {
+  return array.map(({
+            webformatURL,
+            largeImageURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+  }) => `<a class="gallery__item" href="${largeImageURL}"><div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads${downloads}</b>
+    </p>
+  </div>
+</div>` ).join("");
+};
+
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+  borderRadius: '5px',
+  rtl: false,
+  timeout: 3000,
+});
 
